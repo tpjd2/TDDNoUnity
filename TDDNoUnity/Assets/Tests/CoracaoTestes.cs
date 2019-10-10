7776226764ee97ea4b290ac5f1f322cdc7a3d813
SENACEDU\salmo.msjunior <salmo.sjunior@sp.senac.br>
2019-10-10 17:38:26 -0300

    implementação dos primeiros testes

---
+++ TDDNoUnity/Assets/Tests/CoracaoTestes.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using UnityEngine.UI;
+
+namespace Tests
+{
+    public class CoracaoTestes
+    {
+        [Test]
+        public void imagemPreenchimento0Recarregamento0()
+        {
+
+            Image image = new GameObject().AddComponent<Image>();
+            image.fillAmount = 0;
+            Coracao coracao = new Coracao(image);
+            coracao.Recarregar(0);
+            
+            Assert.AreEqual(0, image.fillAmount);
+        }
+
+        [Test]
+        public void imagemPreenchimento0Recarregamento1()
+        {
+
+            Image image = new GameObject().AddComponent<Image>();
+            image.fillAmount = 0;
+            Coracao coracao = new Coracao(image);
+            coracao.Recarregar(1);
+
+            Assert.AreEqual(0.25f, image.fillAmount);
+        }
+
+        private class Coracao
+        {
+            private const float preenchimentoPedaco = 0.25f;
+            private readonly Image image;
+
+            public Coracao(Image image)
+            {
+                this.image = image;
+            }
+
+            internal void Recarregar(int pedacos)
+            {
+                this.image.fillAmount = pedacos * preenchimentoPedaco;
+            }
+        }
+    }
+}
